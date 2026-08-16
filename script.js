772238e594fb6ccb664fbc20dc3ef0c279adadf1
UPT SD Negeri 029 Bentenna <uptsdnegeribentennalutra@gmail.com>
2026-08-16 16:54:03 +0800

    Menambahkan navigasi mobile

---
+++ script.js
@@ -0,0 +1,1 @@
+const toggle=document.querySelector('.menu-toggle');const nav=document.querySelector('#nav');if(toggle&&nav){toggle.addEventListener('click',()=>nav.classList.toggle('open'));nav.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')))}
